9dbbb24c472d28d522581e2a7d4a1eca8bb3cdaa
gnzng <damian@guenzing.de>
2022-12-22 09:33:46 +0100

    Update blocktime.js

--- blocktime.js
+++ blocktime.js
@@ -1,8 +1,9 @@
 const URL = "https://api.blockdate.io/api/now.json";
+
 async function makeAPICall() {
   const result = await fetch(URL);
   result.json().then((data) => {
-    document.getElementById("block").innerHTML = data.blockdate.display;
+    document.getElementById("block").innerHTML = data.blockdate.block;
   });
 }
 
